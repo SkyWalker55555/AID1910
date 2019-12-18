4b2ff38a1cc6f264e70262c7fdd800a118bf3736
Tedu <673668065@qq.com>
2019-12-18 10:36:28 +0800

    init add

---
+++ student.sql
@@ -0,0 +1,196 @@
+-- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: student
+-- ------------------------------------------------------
+-- Server version	5.7.27-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Temporary table structure for view `c2`
+--
+
+DROP TABLE IF EXISTS `c2`;
+/*!50001 DROP VIEW IF EXISTS `c2`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `c2` AS SELECT 
+ 1 AS `name`,
+ 1 AS `sex`,
+ 1 AS `hobby`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `cls`
+--
+
+DROP TABLE IF EXISTS `cls`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cls` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `age` tinyint(3) unsigned NOT NULL,
+  `sex` enum('m','w','o') DEFAULT NULL,
+  `score` float DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `name_index` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cls`
+--
+
+LOCK TABLES `cls` WRITE;
+/*!40000 ALTER TABLE `cls` DISABLE KEYS */;
+INSERT INTO `cls` VALUES (1,'Alex',10,'m',93),(2,'Abby',17,'w',86),(3,'Tom',17,'m',77),(6,'Emma',17,'w',0);
+/*!40000 ALTER TABLE `cls` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hobby`
+--
+
+DROP TABLE IF EXISTS `hobby`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hobby` (
+  `name` varchar(32) NOT NULL,
+  `age` tinyint(3) unsigned NOT NULL,
+  `hobby` set('sing','dance','draw') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hobby`
+--
+
+LOCK TABLES `hobby` WRITE;
+/*!40000 ALTER TABLE `hobby` DISABLE KEYS */;
+INSERT INTO `hobby` VALUES ('Abby',17,'draw'),('Emma',17,'sing');
+/*!40000 ALTER TABLE `hobby` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `interest`
+--
+
+DROP TABLE IF EXISTS `interest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `interest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `hobby` set('sing','dance','draw') NOT NULL,
+  `level` char(3) DEFAULT NULL,
+  `price` decimal(7,2) DEFAULT NULL,
+  `wechat` char(32) DEFAULT NULL,
+  `comment` text,
+  `email` char(28) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `interest`
+--
+
+LOCK TABLES `interest` WRITE;
+/*!40000 ALTER TABLE `interest` DISABLE KEYS */;
+INSERT INTO `interest` VALUES (1,'Joy','sing,dance','B',56800.00,NULL,'骨骼惊奇，练舞奇才',NULL),(2,'Emma','sing','C',8800.00,NULL,'惊为天人，余音绕缭三日不绝于耳',NULL),(3,'Abby','draw','C',8888.88,NULL,'生性愚钝，教不会，建议退学',NULL);
+/*!40000 ALTER TABLE `interest` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `marathon`
+--
+
+DROP TABLE IF EXISTS `marathon`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `marathon` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `athlete` varchar(30) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `registration_time` datetime DEFAULT NULL,
+  `performance` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marathon`
+--
+
+LOCK TABLES `marathon` WRITE;
+/*!40000 ALTER TABLE `marathon` DISABLE KEYS */;
+INSERT INTO `marathon` VALUES (1,'尼古拉斯赵四','1990-02-16','2019-07-31 08:16:29','02:48:56'),(2,'亨利','2000-05-26','2019-08-01 19:26:29','02:24:56'),(3,'曹操','1996-09-01','2019-09-01 20:26:29','02:05:56');
+/*!40000 ALTER TABLE `marathon` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sanguo`
+--
+
+DROP TABLE IF EXISTS `sanguo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sanguo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(24) NOT NULL,
+  `gender` enum('男','女') DEFAULT NULL,
+  `country` enum('魏国','蜀国','吴国') DEFAULT NULL,
+  `attack` smallint(6) DEFAULT NULL,
+  `defense` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sanguo`
+--
+
+LOCK TABLES `sanguo` WRITE;
+/*!40000 ALTER TABLE `sanguo` DISABLE KEYS */;
+INSERT INTO `sanguo` VALUES (1,'司马懿','男','魏国',680,89),(2,'夏侯渊','男','魏国',650,95),(3,'张辽','男','魏国',550,90),(4,'曹操','男','魏国',700,89),(5,'甄姬','女','魏国',670,50),(6,'赵云','男','蜀国',360,68),(7,'孙尚香','女','蜀国',700,51),(8,'诸葛亮','男','蜀国',750,100),(9,'张飞','男','蜀国',710,90),(10,'关羽','男','蜀国',910,95),(11,'周瑜','男','吴国',300,65),(12,'小乔','女','吴国',300,65),(13,'大乔','女','吴国',300,65),(14,'陆逊','男','吴国',300,65),(15,'鲁肃','男','吴国',300,65);
+/*!40000 ALTER TABLE `sanguo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Final view structure for view `c2`
+--
+
+/*!50001 DROP VIEW IF EXISTS `c2`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `c2` AS select `c`.`name` AS `name`,`c`.`sex` AS `sex`,`i`.`hobby` AS `hobby` from (`cls` `c` join `interest` `i`) where (`c`.`name` = `i`.`name`) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-17 10:35:40
